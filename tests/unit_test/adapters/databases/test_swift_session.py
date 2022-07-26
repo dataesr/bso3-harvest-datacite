1ddf920053ee9f223a11a9745803dbfdeec52f01
jova <jonathan.valenca@octo.com>
2022-07-26 10:15:18 +0200

    update config in test_swift

--- tests/unit_test/adapters/databases/test_swift_session.py
+++ tests/unit_test/adapters/databases/test_swift_session.py
@@ -9,7 +9,16 @@ class TestMongoSession(TestCase):
     @patch(f"{TESTED_MODULE}.SwiftService")
     def test_init_and_get_session(self, mock_create_engine):
         # Given
-        config: dict = {}
+        config: dict = {
+            'os_username': 'fake_os_username',
+            'os_password': 'fake_os_password',
+            'os_user_domain_name': 'fake_os_user_domain_name',
+            'os_project_domain_name': 'fake_os_project_domain_name',
+            'os_project_name': 'fake_os_project_name',
+            'os_project_id': 'fake_os_project_id',
+            'os_region_name': 'fake_os_region_name',
+            'os_auth_url': 'fake_os_auth_url',
+        }
 
         #When
         session: SwiftSession = SwiftSession(config)
