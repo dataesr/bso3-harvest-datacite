41edea09f80e6cf84404fde381d402f43b1d8d99
Ketevi <maurice.ketevi@accenture.com>
2022-08-04 09:03:10 +0200

    update import in abstract repository

--- domain/databases/abstract_collection_repository.py
+++ domain/databases/abstract_collection_repository.py
@@ -1,16 +1,14 @@
 from abc import ABCMeta, abstractmethod
-from typing import Collection, Dict
+from pymongo.collection import Collection
 from pymongo.database import Database
 from adapters.databases.mongo_session import MongoSession
-from domain.databases.abstract_session import AbstractSession
-from domain.model.harvest_state import HarvestState
 
 
 class AbstractMongoCollectionRepository(metaclass=ABCMeta):
     session: MongoSession
     database: Database
     collection: Collection
-    
+
     @abstractmethod
     def __init__(self, database: Database):
         raise NotImplementedError
