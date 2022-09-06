51b374009834b5b4a8a35555e0e043abdee7a10a
ANBR <abres@octo.com>
2022-09-06 18:32:04 +0200

    Change cache implementation to use lru_cache from functools

--- adapters/api/affiliation_matcher.py
+++ adapters/api/affiliation_matcher.py
@@ -1,16 +1,28 @@
+from functools import lru_cache
+import unicodedata as ud
 from typing import List
 
 import requests
 from domain.api.abstract_affiliation_matcher import AbstractAffiliationMatcher
-from adapters.api.cache import Cache
-from domain.api.abstract_cache import AbstractCache
+from project.server.main.logger import get_logger
 
+logger = get_logger(__name__)
+
+CACHE_SIZE = 100_000
 
 class AffiliationMatcher(AbstractAffiliationMatcher):
-    def __init__(self, base_url: str, query_cache: AbstractCache):
+    french_publishers = [
+            "Recherche Data Gouv",
+            "Université de Lorraine",
+            "NAKALA",
+            "CIRAD",
+            "INRAE",
+            "DataSuds",
+        ]
+    french_alpha2 = ["fr", "gp", "gf", "mq", "re", "yt", "pm", "mf", "bl", "wf", "tf", "nc", "pf"]
+    def __init__(self, base_url: str):
         self.base_url = base_url
         self.headers = {"Content-type": "application/json"}
-        self.query_cache = query_cache
 
     def get_affiliations_list(self, match_types: List[str], affiliations_list: List[str]):
         return requests.post(
@@ -19,21 +31,40 @@ class AffiliationMatcher(AbstractAffiliationMatcher):
             json={"match_types": match_types, "affiliations": affiliations_list},
         )
 
+    @lru_cache(maxsize=CACHE_SIZE)
     def get_affiliation(self, match_type: str, affiliation_string: str):
-        data = self.query_cache.get(affiliation_string)
-        if not data:
-            data = requests.post(
-                url=f"{self.base_url}/match",
-                headers=self.headers,
-                json={"type": match_type, "query": affiliation_string},
-            ).json()["results"]
-            self.query_cache.add(affiliation_string, data)
-        return data
+        try:
+            return requests.post(
+                    url=f"{self.base_url}/match",
+                    headers=self.headers,
+                    json={"type": match_type, "query": affiliation_string},
+                ).json()["results"]
+        except:
+            logger.exception("Error during get affiliation", exc_info=True)
+
+
+    def _normalizer(self, _str):
+        return ud.normalize("NFKD", _str).encode("ascii", "ignore").decode().lower()
+
+    def is_affiliation_fr(self, creators_affiliations: str,
+                        contributors_affiliations: str, publisher: str, clientId: str):
+        # Implement business rules from https://github.com/Barometre-de-la-Science-Ouverte/bso3-harvest-datacite/blob/dcdump/business_rules.csv
+        # Match for publishers
+        normalized_publisher = self._normalizer(publisher)
+        for french_publisher in self.french_publishers:
+            if self._normalizer(french_publisher) in normalized_publisher:
+                return True
+        # Match for clientId
+        if self._normalizer(clientId).startswith("inist."):
+            return True
+        # Affiliation matcher
+        return len(set(self.french_alpha2) & set(self.get_affiliation("country", creators_affiliations)\
+            + self.get_affiliation("country", contributors_affiliations))) != 0
 
 
 class AffiliationMatcherDemo(AffiliationMatcher):
     def __init__(self):
-        super().__init__(base_url="https://affiliation-matcher.staging.dataesr.ovh", query_cache=Cache())
+        super().__init__(base_url="https://affiliation-matcher.staging.dataesr.ovh")
         self.match_type = ["country"]  # "grid" "rnsr" "ror"
         self.affiliations_list = [
             "Department of Medical Genetics, Hotel Dieu de France, Beirut, Lebanon",
--- tests/unit_test/adapters/api/test_affiliation_matcher.py
+++ tests/unit_test/adapters/api/test_affiliation_matcher.py
@@ -1,18 +1,18 @@
 from unittest.mock import patch
 from unittest import TestCase
 from adapters.api.affiliation_matcher import AffiliationMatcher
-from adapters.api.mock_cache import MockCache
 
 TESTED_MODULE = "adapters.api.affiliation_matcher"
 
 
 class TestAffiliationMatcher(TestCase):
     def setUp(self):
-        self.cache_size = 1
-        self.mock_cache = MockCache(nb_items_max=self.cache_size)
-        self.affiliation_matcher = AffiliationMatcher("fake_url", self.mock_cache)
+        self.affiliation_matcher = AffiliationMatcher("fake_url")
         self.match_type = "match_type"
         self.affiliation_string = "affiliation_string"
+
+    def tearDown(self):
+        self.affiliation_matcher.get_affiliation.cache_clear()
 
     @patch(f"{TESTED_MODULE}.requests.post")
     def test_get_affiliation_first_request(self, mock_post):
@@ -25,8 +25,8 @@ class TestAffiliationMatcher(TestCase):
             headers=self.affiliation_matcher.headers,
             json={"type": self.match_type, "query": self.affiliation_string},
         )
-        assert self.mock_cache.nb_cache_retrieval == 0
-        assert len(self.mock_cache.cache.values()) == 1
+        assert self.affiliation_matcher.get_affiliation.cache_info().hits == 0
+        assert self.affiliation_matcher.get_affiliation.cache_info().currsize == 1
 
     @patch(f"{TESTED_MODULE}.requests.post")
     def test_get_affiliation_cached_request(self, mock_post):
@@ -36,5 +36,5 @@ class TestAffiliationMatcher(TestCase):
         self.affiliation_matcher.get_affiliation(self.match_type, self.affiliation_string)
         # Then
         mock_post.assert_called_once()
-        assert self.mock_cache.nb_cache_retrieval == 1
-        assert len(self.mock_cache.cache.values()) == 1
+        assert self.affiliation_matcher.get_affiliation.cache_info().currsize == 1
+        assert self.affiliation_matcher.get_affiliation.cache_info().hits == 1
