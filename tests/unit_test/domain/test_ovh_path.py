0290c0cdc68a04541780df92dfcc61c0854b6ec5
ANBR <abres@octo.com>
2022-12-02 17:51:44 +0100

    Add tests for OvhPath

--- tests/unit_test/domain/test_ovh_path.py
+++ tests/unit_test/domain/test_ovh_path.py
@@ -1,3 +1,4 @@
+import os
 from unittest.mock import MagicMock, patch
 from unittest import TestCase
 from domain.model.ovh_path import OvhPath
@@ -41,3 +42,38 @@ class TestOvhPath(TestCase):
         # Then
         self.assertNotEqual(str(ovh_path), "a/standard/path")
         self.assertEqual(str(ovh_path), "a\standard/path")
+
+    def test_OvhPath_eq(self):
+        # Given
+        # When
+        ovh_path_0 = OvhPath('a', 'standard', 'path')
+        ovh_path_1 = OvhPath('a/standard', 'path')
+        # Then
+        self.assertNotEqual(ovh_path_0, "a/standard/path")
+        self.assertEqual(str(ovh_path_0), "a/standard/path")
+        self.assertEqual(ovh_path_0, ovh_path_1)
+
+    def test_OvhPath_lt(self):
+        # Given
+        # When
+        ovh_path_0 = OvhPath('a', 'standard', 'path', '0')
+        ovh_path_1 = OvhPath('a', 'standard', 'path', '1')
+        # Then
+        self.assertTrue(ovh_path_0 < ovh_path_1)
+        self.assertTrue(ovh_path_0 < "a/standard/path/1")
+
+    def test_OvhPath_gt(self):
+        # Given
+        # When
+        ovh_path_0 = OvhPath('a', 'standard', 'path', '0')
+        ovh_path_1 = OvhPath('a', 'standard', 'path', '1')
+        # Then
+        self.assertFalse(ovh_path_0 > ovh_path_1)
+        self.assertFalse(ovh_path_0 > "a/standard/path/1")
+
+    def test_OvhPath_lo_local(self):
+        # Given
+        # When
+        ovh_path = OvhPath('a', 'standard', 'path')
+        # Then
+        self.assertEqual(ovh_path.to_local(), os.path.join('a', 'standard', 'path'))
